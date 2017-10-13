3dfc538b7da86a72e5d434aec484721b23bc2fd2
snowyyu <731444920@qq.com>
2017-10-13 14:28:43 +0800

    crm的token认证过期的码值为110，删除了会员详情中的部分字段

--- src/services/myHttp/myhttpClient.interceptor.ts
+++ src/services/myHttp/myhttpClient.interceptor.ts
@@ -29,8 +29,8 @@ export class MyHttpClientInterceptor implements HttpInterceptor {
         if (event instanceof HttpResponse) {
           if (event.ok) {
             let body = event.body;
-            if (typeof body === 'object') {
-              if (body.status == '411') {
+            if (body&&typeof body === 'object') {
+              if (body.status == '110') {
                 if(!this.router.isActive('/signin',false)){
                   this.toaster.error('', body.message || '登录超时，请重新登录！');
                   this.router.navigate(['/signin']);
