65c0867a4aec972ef1952ef93b25a1f75a18984f
snowyyu <731444920@qq.com>
2017-10-18 18:53:28 +0800

    remove item flash

--- src/app/member-m/member-manage/new-apply/new-apply.component.ts
+++ src/app/member-m/member-manage/new-apply/new-apply.component.ts
@@ -34,6 +34,8 @@ export class NewApplyComponent implements OnInit {
 	borrowHowlong		//借款周期
 	productRemark		//产品简介
 
+	checkApplyExist:boolean=false
+
 	constructor(
 		private route:ActivatedRoute,
 		private router:Router,
@@ -74,6 +76,7 @@ export class NewApplyComponent implements OnInit {
 	}
 
 	selectProduct(id){
+		this.checkApplyExist=false
 		this.productTypeName=this.newData[id].productTypeName
 		console.log(this.productTypeName)
 		console.log(this.newData)
@@ -82,6 +85,7 @@ export class NewApplyComponent implements OnInit {
 		this.productRemark=this.newData[id].productRemark		//产品简介
 		this.newApply.checkApplyExist(0,this.memberId,id)
 			.then(res=>{
+				this.checkApplyExist=true
 				console.log(res)
 			})
 			.catch(res=>{
