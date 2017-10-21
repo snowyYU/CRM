6fa3d24cf8522f1799368476cb4030361862a515
snowyyu <731444920@qq.com>
2017-10-21 18:09:39 +0800

    edit yuanfarmat pipe

--- src/pipe/yuan-format/yuan-format.pipe.ts
+++ src/pipe/yuan-format/yuan-format.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 	name: 'yuanFormat'
 })
 export class YuanFormatPipe implements PipeTransform {
-	transform(value: any, dot:number): any {
+	transform(value: any, dot:number,noUnit:boolean): any {
 		if(value&&value!="--") {
 			dot = dot > 0 && dot <= 20 ? dot : 2; 
 			value = parseFloat((value + "").replace(/[^\d\.-]/g, "")).toFixed(dot) + ""; 
@@ -13,10 +13,10 @@ export class YuanFormatPipe implements PipeTransform {
 			for (let i = 0; i < l.length; i++) { 
 			t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : ""); 
 			} 
-			return "￥"+t.split("").reverse().join("") + "." + r; 
+			return noUnit?t.split("").reverse().join("") + "." + r:"￥"+t.split("").reverse().join("") + "." + r; 
 		} else {
 			if (value==0) {
-				return "￥0.00"
+				return noUnit?"0.00":"￥0.00"
 			}else{
 				return "--"
 			}
