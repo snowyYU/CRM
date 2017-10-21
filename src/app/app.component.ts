81133bef36922209d3d89b302cc38543768f7425
252352801@qq.com <252352801@qq.com>
2017-10-21 10:27:39 +0800

    页面跳转滚动条回到顶部；加页脚

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router, ActivatedRoute, NavigationEnd} from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -6,7 +7,34 @@ import { Component } from '@angular/core';
   providers:[]
 })
 export class AppComponent {
-  constructor(){
+  constructor(
+    private actRoute: ActivatedRoute,
+    private router: Router
+  ){
+    this.subscribeRouterChange();
   }
 
+
+  private subscribeRouterChange() {
+    this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .map(() => this.actRoute)
+      .map(route => {
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      })
+      .filter(route => route.outlet === 'primary')  // 过滤出未命名的outlet，<router-outlet>
+      //.mergeMap(route => route.data)                // 获取路由配置数据
+      .subscribe((event) => {
+        {
+          if(document.documentElement){
+            document.documentElement.scrollTop=0;
+          }else if(document.body){
+            document.body.scrollTop=0;
+          }
+        }
+      });
+  }
 }
