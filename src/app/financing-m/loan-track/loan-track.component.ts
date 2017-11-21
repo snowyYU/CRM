4a30f76177cce91ed4fec6f87fcb7a53dcd35a28
wittyMouse <505357658@qq.com>
2017-11-21 19:16:02 +0800

    开放融资管理在贷跟踪

--- src/app/financing-m/loan-track/loan-track.component.ts
+++ src/app/financing-m/loan-track/loan-track.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router'
+import { Router,ActivatedRoute,ParamMap } from '@angular/router'
 import { PopService } from 'dolphinng'
 import { LoanTrackService,SendData } from './loan-track.service'
 import { DateService } from '../../../services/date/date.service'
@@ -41,70 +41,133 @@ export class LoanTrackComponent implements OnInit {
 	status=0                  //状态
 
 	repaymentPlanList:any[]   //还款计划列表
+
+	thisPageRoute:string='financingM/loanTrack'
+
 	constructor(
 		public authRole:AuthRoleService,
 		private router:Router,
+		private route:ActivatedRoute,
 		private pop:PopService,
 		private loanTrack:LoanTrackService,
 		private dateService:DateService
 		) {}
 
 	ngOnInit() {
-		this.getLoanList()
-		this.getAllApp()
-		this.getProductsList('0')
+		// this.getLoanList()
+		// this.getAllApp()
+		// this.getProductsList('0')
 		//格式化并获取当前日期
-		this.todayDate=this.dateService.format({
-			date:this.dateService.todayDate(),
-			formatType:"yyyy-MM-dd"
+		
+		// this.todayDate=this.dateService.format({
+		// 	date:this.dateService.todayDate(),
+		// 	formatType:"yyyy-MM-dd"
+		// })
+		this.subscribeRouteParams()
+	}
+
+	//11.14,新增两个方法，
+	//路由中的参数和获取列表的请求参数是否要一样呢
+	//有些时候是一样的，有时候又不是，不过目前系统中是一样的
+	//规范化，1.从组件的service中导入查询条件的interface。2.组件内部定义路由的参数结构，两者不一定相同
+	//必传参数在声明时必须初始化
+	subscribeRouteParams(){
+		this.route.paramMap.subscribe((paramMap:ParamMap)=>{
+			console.log(paramMap)
+			console.log(paramMap['params']['rows'])
+			console.log(!!paramMap['params'])
+			
+				// if (paramMap['params']['rows']) {
+				// 	this.rows=paramMap['params']['rows']
+				// }
+				paramMap['params']['rows']?this.rows=parseInt(paramMap['params']['rows']):null
+				paramMap['params']['page']?this.page=parseInt(paramMap['params']['page']):null
+				paramMap['params']['status']?this.status=paramMap['params']['status']:null
+				paramMap['params']['borrowApplyId']?this.borrowApplyId=paramMap['params']['borrowApplyId']:null
+				paramMap['params']['keyword']?this.companyName=paramMap['params']['keyword']:null
+
+				// paramMap['params']['rows']?this.rows=paramMap['params']['rows']:null
+				// paramMap['params']['rows']?this.rows=paramMap['params']['rows']:null
+			
+
+			this.getLoanList()
 		})
 	}
 
-	//获取归属渠道下拉列表数据
-	getAllApp(){
-		this.loanTrack.getAllApp()
-			.then(res=>{
-				console.log(res)
-				this.resourceList=res.body.records
-				this.resourceList.unshift({resourceId:'0',resourceName:'全部'})
-				this.resourceId=this.resourceList[0].resourceId
-				// this.getProductsList(this.appId)
-			})
-			.catch(res=>{
-				this.pop.error({
-					title:'错误提示',
-					text:res.message
-				})
-			})
+	navigate(){
+		let routeParam:{
+			page,
+			rows,
+			status,
+			borrowApplyId?,
+			keyword?
+		}={
+			page:this.page,
+			rows:this.rows,
+			status:this.status
+		}
+
+		if (this.borrowApplyId) {
+			routeParam.borrowApplyId=this.borrowApplyId
+		}
+
+		if (this.companyName) {
+			routeParam.keyword=this.companyName
+		}
+
+		console.log("router",this.router)
+		console.log("activerouter",this.route)
+
+		this.router.navigate([this.thisPageRoute,routeParam])
+
 
 	}
+
+	//获取归属渠道下拉列表数据
+	// getAllApp(){
+	// 	this.loanTrack.getAllApp()
+	// 		.then(res=>{
+	// 			console.log(res)
+	// 			this.resourceList=res.body.records
+	// 			this.resourceList.unshift({resourceId:'0',resourceName:'全部'})
+	// 			this.resourceId=this.resourceList[0].resourceId
+	// 			// this.getProductsList(this.appId)
+	// 		})
+	// 		.catch(res=>{
+	// 			this.pop.error({
+	// 				title:'错误提示',
+	// 				text:res.message
+	// 			})
+	// 		})
+
+	// }
 
 	//获取贷款产品下拉列表数据
-	getProductsList(appId){
-		if(appId=='0'){
-			let product:Product={
-				productId:'0',
-				productName:'全部'
-			}
-			let productList:Product[]=[product]
-			this.productList=productList
-			this.productId=this.productList[0].productId
-			return
-		}
-		this.loanTrack.getProductsList(appId)
-			.then(res=>{
-				console.log(res)
-				this.productList=res.body.records
-				this.productList.unshift({productId:'0',productName:'全部'})
-				this.productId=res.body.records[0].productId
-			})
-			.catch(res=>{
-				this.pop.error({
-					title:'错误提示',
-					text:res.message
-				})
-			})
-	}
+	// getProductsList(appId){
+	// 	if(appId=='0'){
+	// 		let product:Product={
+	// 			productId:'0',
+	// 			productName:'全部'
+	// 		}
+	// 		let productList:Product[]=[product]
+	// 		this.productList=productList
+	// 		this.productId=this.productList[0].productId
+	// 		return
+	// 	}
+	// 	this.loanTrack.getProductsList(appId)
+	// 		.then(res=>{
+	// 			console.log(res)
+	// 			this.productList=res.body.records
+	// 			this.productList.unshift({productId:'0',productName:'全部'})
+	// 			this.productId=res.body.records[0].productId
+	// 		})
+	// 		.catch(res=>{
+	// 			this.pop.error({
+	// 				title:'错误提示',
+	// 				text:res.message
+	// 			})
+	// 		})
+	// }
 
 	//获取在贷跟踪列表数据
 	getLoanList(){
@@ -114,10 +177,11 @@ export class LoanTrackComponent implements OnInit {
 			rows:this.rows,
 			borrowApplyId:this.borrowApplyId,
 			companyName:this.companyName,
-			approveAmount:this.approveAmount,
-			productName:this.productName,
-			repaymentWay:this.repaymentWay,
-			remarks:this.remarks
+			// approveAmount:this.approveAmount,
+			// productName:this.productName,
+			// repaymentWay:this.repaymentWay,
+			// remarks:this.remarks
+			status:this.status
 		}
 
 		this.loanTrack.getLoanList(sendData)
